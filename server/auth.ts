7a576a439ad94ba23b20fc1a527e0465b78b20c3
Milosz Olejnik <milosz.olejnik@tuta.io>
2024-09-03 14:01:51 +0200

    added next-auth for google and github and sync it with drizzle

---
+++ server/auth.ts
@@ -0,0 +1,24 @@
+import NextAuth from 'next-auth';
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
+import Google from 'next-auth/providers/google';
+import Github from 'next-auth/providers/github';
+
+import { db } from '@/server';
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: DrizzleAdapter(db),
+  secret: process.env.AUTH_SECRET,
+  session: {
+    strategy: 'jwt',
+  },
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Github({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+});
